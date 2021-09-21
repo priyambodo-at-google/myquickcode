094255f0bd279ca6f6c2db8cb7a7df73eb9a1e73
Doddi Priyambodo <priyambodo@google.com>
2021-09-21 20:31:09 +0700

    regular commit

---
+++ cariDiskon.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class cariDiskon
+{
+    public static void main (String []args)
+    {
+        int vPersen = 0;
+        double vHargaBayar = 0;
+        double vHargaDiskon = 0;
+
+        Scanner vInput = new Scanner(System.in);
+
+        System.out.print("Berapa jumlah barang yang dibeli? ");
+        double vBanyak = vInput.nextDouble();
+
+        System.out.print("Berapa harga satuan dari barang tersebut? ");
+        double vSatuan = vInput.nextDouble();
+
+        double vTotal = (vBanyak * vSatuan);
+
+        if(vTotal < 100000)
+        {
+            vPersen = 0;
+        }
+        else if(vTotal >= 100001 && vTotal < 200000)
+        {
+            vPersen = 5;
+        }
+        else if(vTotal >= 200001 && vTotal < 300000)
+        {
+            vPersen = 10;
+        }
+        else if(vTotal >= 300001 && vTotal < 400000)
+        {
+            vPersen = 15;
+        }
+        else if(vTotal >= 400001 && vTotal < 500000)
+        {
+            vPersen = 20;
+        }
+        else if (vTotal > 500000){
+            vPersen = 25;
+        }
+        
+        double vDiskon = (vTotal * vPersen / 100) ;
+        double vFinal = (vTotal - vDiskon);
+
+        System.out.print("Total Pembelian = " + vTotal + "\n");
+        System.out.print("Jumlah Diskon = " + vDiskon + "\n");
+        System.out.print("Harga Final setelah dikurang diskon = " + vFinal + "\n");
+
+        System.out.print("Masukkan rupiah pembayaran anda = ");
+        double vBayar = vInput.nextDouble();
+
+        if (vBayar == vFinal)
+        {
+            System.out.print("Uang Pas \n");
+        }
+            else 
+            {
+                System.out.print("Kembalian anda adalah = " + (vBayar-vFinal) + "\n"); 
+            };
+    }
+}
